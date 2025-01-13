805cae17531bd803e4eee0b2cb177810fc2ab2c9
Jeff Johnson <1198065+jefdaj@users.noreply.github.com>
2025-01-12 19:41:11 -0800

    misc minor cleanup

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,17 @@
 FROM python:3.9-bullseye AS base
+
 RUN apt update && apt-get install -y \
     libgmp-dev \
     libmpfr-dev \
     libmpc-dev \
     graphviz
+
+RUN apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
 RUN pip install 'poetry==1.1.13'
+
 ENV POETRY_REQUESTS_MAX_RETRIES=25
-WORKDIR /electionguard-python
+
+RUN mkdir /repo
+WORKDIR /repo
--- makefile-docker-env.sh
+++ makefile-docker-env.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # Usage:
-# 0. install Docker
-# 1. run this to spin up a dev container
-# 2. in the container, use `make` commands as described in the README
-#    (except targets using docker compose; those run outside)
+# 0. Install Docker.
+# 1. Run this to spin up a dev container.
+# 2. In the container, use `make` commands as described in the README
+#    (except targets using docker compose; those run outside).
 
 image_name=electionguard-python-makefile-docker-env
 
 docker build -t $image_name .
 
 docker run -it \
-  --mount type=bind,src=.,dst=/electionguard-python \
+  --mount type=bind,src=.,dst=/repo \
   --mount type=bind,src=./data,dst=/data \
   $image_name bash
