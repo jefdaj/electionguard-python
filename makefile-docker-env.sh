100160fd3ae4977821322cf951016b8cfd46d7bd
Jeff Johnson <1198065+jefdaj@users.noreply.github.com>
2025-01-12 16:48:43 -0800

    docker-dev.sh -> makefile-docker-env.sh

--- makefile-docker-env.sh
+++ makefile-docker-env.sh
@@ -4,6 +4,7 @@
 # 0. install Docker
 # 1. run this to spin up a dev container
 # 2. in the container, use `make` commands as described in the README
+#    (except targets using docker compose; those run outside)
 
 image_name=electionguard-python-make-env
 
