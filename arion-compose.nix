1566f0423b2a51fa5971a7afbc7f7396e08a9800
Jeff Johnson <1198065+jefdaj@users.noreply.github.com>
2025-01-22 09:32:47 -0800

    old api containers work

--- arion-compose.nix
+++ arion-compose.nix
@@ -1,6 +1,25 @@
-{ pkgs, ... }:
+{ pkgs, ...}:
 
 let
+  # common JSON config file for nix + haskell code
+  # TODO is this the best way to do it?
+  testConfig = builtins.fromJSON (builtins.readFile ./test-config.json);
+
+  mkApiVm = mode: port: {
+
+    service.image = "electionguard/electionguard-web-api:latest";
+
+    service.environment.API_MODE = mode;
+
+    # TODO are ports 80 and 8100 actually exposed, or only to the host system? how to close them?
+    service.environment.PORT = port;
+    # service.expose = []; # [ ( builtins.toString port) ];
+    service.ports  = [ ((builtins.toString port) + ":" + (builtins.toString port)) ];
+
+    # service.volumes = [ "${toString ./.}/postgres-data:/var/lib/postgresql/data" ];
+
+  };
+
   # make one vm entry, suitable for merging into the main services attrset
   mkApiVmAttrs = mode: startPort: n: {
     name = mode + builtins.toString n;
@@ -8,7 +27,7 @@ let
   };
 
   # make a list of vm entries with the same mode, suitable for merging into the main services attrset
-  mkApiVmAttrList = mode: startPort: nVms: map (mkApiVmAttrs mode startPort) (nixpkgs.lib.range 1 nVms);
+  mkApiVmAttrList = mode: startPort: nVms: map (mkApiVmAttrs mode startPort) (pkgs.lib.range 1 nVms);
 
   # make the entire services attrset
   # the start ports are arbitrary
@@ -18,5 +37,5 @@ let
 
 in {
   config.project.name = "electionguard-web-api-test";
-  services = mkServices testConfig;
+  config.services = mkServices testConfig;
 }
