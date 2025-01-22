77b4902fcef62a0036605db139fdc14b79876120
Jeff Johnson <1198065+jefdaj@users.noreply.github.com>
2025-01-22 10:30:21 -0800

    simplify ports a bit

--- arion-compose.nix
+++ arion-compose.nix
@@ -3,33 +3,23 @@
 let
   testConfig = builtins.fromJSON (builtins.readFile ./test-config.json);
 
-  mkApiVm = mode: n:
-  let
-    hostApiPort = (if mode == "guardian" then 8100 else 8200) + n;
-  in {
-
+  mkApiVm = mode: port:
+  {
     service.image = "electionguard/electionguard-web-api:latest";
-
     service.environment.API_MODE = mode;
-
-    service.environment.PORT = hostApiPort;
+    service.environment.PORT = port;
     service.ports  = [
       # host:container
-      (builtins.toString hostApiPort + ":" + builtins.toString hostApiPort)
+      (builtins.toString port + ":" + builtins.toString port)
     ];
-
-    # this would expose them to other computers?
-    # service.expose = [ ( builtins.toString hostApiPort) ];
-    # service.expose = [];
-
+    # service.expose = [ ( builtins.toString port) ];
     # service.volumes = [ "${toString ./.}/postgres-data:/var/lib/postgresql/data" ];
-
   };
 
-  # make one vm entry, suitable for merging into the main services attrset
+  # make a single-vm attrset suitable for merging into the main services attrset
   mkApiVmAttrs = mode: startPort: n: {
     name = mode + builtins.toString n;
-    value = mkApiVm mode n;
+    value = mkApiVm mode (startPort + n);
   };
 
   # make a list of vm entries with the same mode, suitable for merging into the main services attrset
