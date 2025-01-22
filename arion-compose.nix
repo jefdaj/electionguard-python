e15896e2dacedd63e43ea17752071eac17c27ffb
Jeff Johnson <1198065+jefdaj@users.noreply.github.com>
2025-01-22 13:19:27 -0800

    simpler fn names

--- arion-compose.nix
+++ arion-compose.nix
@@ -1,9 +1,10 @@
 { pkgs, ...}:
 
 let
+
   testConfig = builtins.fromJSON (builtins.readFile ./test-config.json);
 
-  mkApiVm = mode: port:
+  mkContainer = mode: port:
   {
     service.image = "electionguard/electionguard-web-api:1.0.4"; # latest
     service.environment.API_MODE = mode;
@@ -17,19 +18,19 @@ let
   };
 
   # make a single-vm attrset suitable for merging into the main services attrset
-  mkApiVmAttrs = mode: startPort: n: {
+  mkAttrs = mode: startPort: n: {
     name = mode + builtins.toString n;
-    value = mkApiVm mode (startPort + n);
+    value = mkContainer mode (startPort + n);
   };
 
-  # make a list of vm entries with the same mode, suitable for merging into the main services attrset
-  mkApiVmAttrList = mode: startPort: nVms: map (mkApiVmAttrs mode startPort) (pkgs.lib.range 1 nVms);
+  # make a list of vm attrsets with the same mode, suitable for merging into the main services attrset
+  mkAttrsList = mode: startPort: nVms: map (mkAttrs mode startPort) (pkgs.lib.range 1 nVms);
 
   # make the entire services attrset
   # the start ports are arbitrary
   mkServices = cfg:
-    builtins.listToAttrs (mkApiVmAttrList "guardian" cfg.guardianStartPort cfg.nGuardians) //
-    builtins.listToAttrs (mkApiVmAttrList "mediator" cfg.mediatorStartPort cfg.nMediators);
+    builtins.listToAttrs (mkAttrsList "guardian" cfg.guardianStartPort cfg.nGuardians) //
+    builtins.listToAttrs (mkAttrsList "mediator" cfg.mediatorStartPort cfg.nMediators);
 
 in {
   config.project.name = "electionguard-web-api-test";
